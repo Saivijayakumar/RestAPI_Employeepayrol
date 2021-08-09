e11d12683d6634a02df59820acfbe674122f55a2
Sai Vijayakumar Bollu <saivijayakumar.b@tvsnext.io>
2021-08-09 21:15:29 +0530

    [SaiVijayaKumar] Getting The data From Json Server

--- RestSharpTesting/EmployeeModel.cs
+++ RestSharpTesting/EmployeeModel.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace RestSharpTesting
 {
-    class EmployeeModel
+    public class EmployeeModel
     {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int salary { get; set; }
     }
 }
--- RestSharpTesting/RestSharpForEmploye.cs
+++ RestSharpTesting/RestSharpForEmploye.cs
@@ -1,14 +1,42 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using RestSharp;
 using System;
+using System.Collections.Generic;
+using System.Net;
 
 namespace RestSharpTesting
 {
     [TestClass]
     public class RestSharpForEmploye
     {
-        [TestMethod]
-        public void TestMethod1()
+        //seting the restclient as null
+        RestClient client = null;
+        [TestInitialize]
+        public void SetUp()
         {
+            client = new RestClient("http://localhost:3000");
+        }
+        public IRestResponse GetAllEmployees()
+        {
+            //Define method Type
+            RestRequest request = new RestRequest("/employees", Method.GET);
+            //Eexcute request
+            IRestResponse response = client.Execute(request);
+            //Return the response
+            return response;
+        }
+        //UC 1: Getting all the employee details from json server
+        [TestMethod]
+        public void OnCallingGetMethodWeAreReturningEmployeesData()
+        {
+            IRestResponse response = GetAllEmployees();
+            //Deserialize json object to List
+            var jsonObject = JsonConvert.DeserializeObject<List<EmployeeModel>>(response.Content);
+            //Check by count 
+            Assert.AreEqual(6, jsonObject.Count);
+            //Check by status code 
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
         }
     }
 }
