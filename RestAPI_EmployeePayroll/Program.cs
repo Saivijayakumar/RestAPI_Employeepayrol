dcc0ebaec3bfc94b963abf5b69b1c7bd9d8a526a
Sai Vijayakumar Bollu <saivijayakumar.b@tvsnext.io>
2021-08-09 20:44:12 +0530

    [SaiVijayakumar] commit welcome Message

---
+++ RestAPI_EmployeePayroll/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestAPI_EmployeePayroll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Program using Rest Sharp");
+        }
+    }
+}
---
+++ RestSharpTesting/RestSharpForEmploye.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace RestSharpTesting
+{
+    [TestClass]
+    public class RestSharpForEmploye
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
